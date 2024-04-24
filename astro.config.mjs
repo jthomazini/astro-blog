85507767ec102db99fe501ec7c89881bcba2fdf5
Joao Thomazini <joao.thomazini@gmail.com>
2024-04-24 15:44:36 -0400

    Finished?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://master--astro-jblog.netlify.app/',
   integrations: [preact()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
